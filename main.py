6215c5e9e5438a96ce84adc23476118b728c31a7
Terrence Brannon <metaperl@gmail.com>
2023-09-22 09:11:39 -0400

    Refactor app into class.

--- main.py
+++ main.py
@@ -1,53 +1,63 @@
 from nicegui import ui
 
+from traitlets import HasTraits, Int, Unicode, default, Set, List, Any
+from traitlets.config import Application
+
 from feelings_and_needs.nycnvc.lists import feelings
 
-needs = list()
-chosen_feelings = set()
-feeling_phrase = "feeling"
 
-ui.label('I AM')
+class MyApp(Application):
+    needs = List()
+    chosen_feelings = Set()
+    feeling_phrase = Unicode("feeling")
+    ui_sentence = Any()
+    sentence = Unicode()
 
-def store_feeling_phrase(p):
-    global feeling_phrase
-    print(f"Storing {p}")
-    feeling_phrase = p
-    generate_sentence()
+    def build_ui(self):
+        ui.label('I AM')
 
-def add_feeling(p):
-    global chosen_feelings
-    print(f"Storing {p}")
-    chosen_feelings.add(p)
-    generate_sentence()
+        feeling_toggle = ui.toggle(
+            ["feeling", "guessing you are feeling"],
+            value="feeling",
+            on_change=lambda e: self.store_feeling_phrase(e.value)
+        )
 
-feeling_toggle = ui.toggle(
-    ["feeling", "guessing you are feeling"],
-    value="feeling",
-    on_change=lambda e: store_feeling_phrase(e.value)
-)
+        selects = list()
 
-selects = list()
+        for f in "negative positive".split():
+            with ui.row():
+                ui.label(f'{f} Feelings')
+                for _ in feelings[f]:
+                    print(f"--> Currently working with {_}")
+                    with ui.column():
+                        ui.label(_[0].upper())
+                        selects.append(
+                            ui.select(_, on_change=lambda e: self.add_feeling(e.value))
+                        )
 
-for f in "negative positive".split():
-    with ui.row():
-        ui.label(f'{f} Feelings')
-        for _ in feelings[f]:
-            print(f"--> Currently working with {_}")
-            with ui.column():
-                ui.label(_[0].upper())
-                selects.append(
-                    ui.select(_, on_change=lambda e: add_feeling(e.value))
-                )
+        self.ui_sentence = ui.label()
+
+    def store_feeling_phrase(self, p):
+        print(f"Storing {p}")
+        self.feeling_phrase = p
+        self.generate_sentence()
+
+    def add_feeling(self, p):
+        print(f"Storing {p}")
+        self.chosen_feelings.add(p)
+        self.generate_sentence()
+
+    def generate_sentence(self):
+        feelings_joined = ", ".join(list(self.chosen_feelings))
+        print(f"in gen sen, {self.feeling_phrase=}. {feelings_joined=} ")
+
+        sentence = ["I am ", self.feeling_phrase, " ", feelings_joined]
+        self.ui_sentence.set_text(sentence)
+
+    def start(self):
+        self.build_ui()
+        ui.run()
 
 
-
-def generate_sentence():
-    global feeling_phrase
-    feelings_joined = ", ".join(list(chosen_feelings))
-    print(f"in gen sen, {feeling_phrase=}. {feelings_joined=} ")
-
-    sentence = ["I am ", feeling_phrase, " ", feelings_joined]
-    nvc_sentence.set_text(sentence)
-
-nvc_sentence = ui.label()
-ui.run()
+if __name__ in {"__main__", "__mp_main__"}:
+    MyApp.launch_instance()
