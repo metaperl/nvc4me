99d1852409b63cd9eee9ab4e03a2ec4a7672062d
Terrence Brannon <metaperl@gmail.com>
2023-10-26 11:08:48 -0400

    now we get the currently selected feelings accurately

--- toy_panel.py
+++ toy_panel.py
@@ -1,4 +1,5 @@
 import panel as pn
+from loguru import logger
 
 feelings = dict()
 positive_feelings = list()
@@ -71,19 +72,81 @@ revulsion""".split())
 feelings['positive'] = positive_feelings
 feelings['negative'] = negative_feelings
 
-i_am = pn.pane.Markdown("I AM").servable()
+_feeling_expr = 'feeling'  # a _feeling_expr is "feeling" or "guessing that you are feeling"
+feelings_set = set()  # the feelings set comes from selecting values in the MultiSelect
+
+ui_sentence = pn.widgets.TextAreaInput(auto_grow=True)
+
+pn.pane.Markdown("# Let's Build an NVC Sentence")
+
+i_am = pn.pane.Markdown("### I am").servable()
+
+
+def build_sentence(feeling_expr, feeling_words):
+    _ = ', '.join(feeling_words)
+    sentence = "I am " + feeling_expr + " " + _
+    return sentence
+
+
+def update_ui_sentence():
+    _ = build_sentence(_feeling_expr, list(feelings_set))
+    ui_sentence.value = _
+
+
+def update_feeling_expr(feeling_subject):
+    global _feeling_expr
+    _feeling_expr = feeling_subject
+    print(f"feeling expr {_feeling_expr}")
+    update_ui_sentence()
+
+
+def clear_related_feelings(selected_feelings):
+    global feelings_set
+    print("--------- BEGIN clear_related_feelings")
+    print(f"{feelings_set=}")
+    for feeling_tone in feelings:
+        for _ in feelings[feeling_tone]:
+            for selected_feeling in selected_feelings:
+                if selected_feeling in _:
+                    for possible_former_feeling in _:
+                        feelings_set.discard(possible_former_feeling)
+    print(f"After clearing related feelings, {feelings_set=}")
+    print("--------- END clear_related_feelings")
+
+
+def update_feelings_set(selected_feelings):
+    global feelings_set
+    print(f"current pulldown is providing {selected_feelings=}")
+
+    clear_related_feelings(selected_feelings)
+
+    for _ in selected_feelings:
+        feelings_set.add(_)
+
+    print(f"After updating {feelings_set=}")
+    print(f"After updating {feelings_set_history=}")
+    print("=============================================")
+
+    update_ui_sentence()
+
+
 feeling_toggle = pn.widgets.RadioButtonGroup(
     name='Radio Button Group', options=['feeling', 'guessing you are feeling'],
-    button_type='success').servable()
+    button_type='success')
+
+sentence_reactor = pn.bind(update_feeling_expr, feeling_toggle)
+
+pn.Column(feeling_toggle, sentence_reactor).servable()
 
 for f in feelings:
     print(f"FEELING {f}")
     pn_row = pn.Row(f'### {f} feelings:', scroll=False).servable()
     for list_of_feelings in feelings[f]:
-        print(f"--> Currently working with {list_of_feelings}")
+        # print(f"--> Currently working with {list_of_feelings}")
         card_label = list_of_feelings[0].upper()
         card_select = pn.widgets.MultiSelect(options=list_of_feelings)
-        card = pn.Column(f'### {card_label}', card_select)
+        multiselect_reactor = pn.bind(update_feelings_set, card_select)
+        card = pn.Card(pn.Column(card_select, multiselect_reactor), title=card_label, collapsed=True)
         pn_row.append(card)
 
-ui_sentence = pn.widgets.TextAreaInput(auto_grow=True).servable()
+pn.Column('# Here is our current NVC Sentence', ui_sentence).servable()
